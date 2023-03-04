508ff49966855e63808162837f1eb2f125b8c8e8
bonsall2004 <70919257+bonsall2004@users.noreply.github.com>
2023-03-04 17:48:50 +0000

    Updated Dependencies
    Changed the npm i discord.js to npm i discord.js@13.6.0 to avoid issues when using it now later versions have been released

--- installdep.bat
+++ installdep.bat
@@ -1,2 +1,2 @@
 @echo off
-IF EXIST package.json ( npm i discord.js && npm i fs && npm i nodemon && pause && del installdep.bat ) ELSE ( echo There is no package file, run the init.bat file first && pause && del installdep.bat ) 
+IF EXIST package.json ( npm i discord.js@13.6.0 && npm i fs && npm i nodemon && pause && del installdep.bat ) ELSE ( echo There is no package file, run the init.bat file first && pause && del installdep.bat ) 
